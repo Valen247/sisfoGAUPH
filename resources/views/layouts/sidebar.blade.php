a5b1e30852a619d0801285b0cd2872b4a9acacbc
Alvin Valencia <raverazor@gmail.com>
2022-11-26 13:31:56 +0700

    lol

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,12 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+         <li class="nav-item">
+            <a href="/admin/cetegory" class="nav-link">
+              <i class="nav-icon fas fa-eye"></i>
+              <p>
+                Categories
+               </p>
+            </a>
+          </li>
+</ul>
